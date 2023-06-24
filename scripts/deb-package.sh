2101455e0a17c855b62c27641916fede4afe979f
Benjamin Kearns <benjamin.kearns@gmail.com>
2023-06-25 05:35:48 +1200

    Fix libsoup version

--- scripts/deb-package.sh
+++ scripts/deb-package.sh
@@ -9,7 +9,7 @@ Version: `cat src/resources/version`
 Architecture: `dpkg --print-architecture`
 Essential: no
 Maintainer: Forensant
-Depends: libc6 (>= 2.2.1), libgtk-3-0, libpython3.11, libsoup3.0-0, libjson-glib-1.0-0, libwebkit2gtk-4.1-0, libgtksourceview-3.0-1, libgee-0.8-2, libnotify4
+Depends: libc6 (>= 2.2.1), libgtk-3-0, libpython3.11, libsoup-3.0-0, libjson-glib-1.0-0, libwebkit2gtk-4.1-0, libgtksourceview-3.0-1, libgee-0.8-2, libnotify4
 Description: Native next generation intercepting proxy, designed for security testing of web applications.
 EOL
 
