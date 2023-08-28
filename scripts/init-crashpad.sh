0c2460bdda602919cafa3083755d6d22223ac6ee
Benjamin Kearns <benjamin.kearns@gmail.com>
2023-08-29 08:27:24 +1200

    Fix crashpad on flatpak and upload to Sentry

--- scripts/init-crashpad.sh
+++ scripts/init-crashpad.sh
@@ -2,16 +2,25 @@
 
 # This script is used to build the crashpad handler for the current platform.
 
+export PATH=$PATH:/usr/lib/sdk/llvm16/bin
+echo $PATH
+
 mkdir -p builddir
 
+echo "Checking out submodules"
 git submodule update --init --recursive
 cd subprojects
 mkdir crashpad
 cd crashpad
+
+echo "Fetching crashpad"
 ../depot_tools/fetch crashpad
 cd crashpad
+
+echo "Generating build files"
 ../../depot_tools/gn gen out/Default
 
+echo "Modifying the build files"
 if ! grep -q "include <stdint.h>" "third_party/mini_chromium/mini_chromium/base/logging.h"; then
     sed -i '10 a #include <stdint.h>' third_party/mini_chromium/mini_chromium/base/logging.h
 fi
@@ -25,6 +34,9 @@ if ! grep -q "include <stdint.h>" "third_party/mini_chromium/mini_chromium/base/
     sed -i '10 a typedef unsigned char uint8_t;' third_party/mini_chromium/mini_chromium/base/third_party/icu/icu_utf.h
 fi
 
+echo "Starting build"
 ../../depot_tools/ninja -C out/Default
 cd ../../..
+
+echo "Copying output"
 cp subprojects/crashpad/crashpad/out/Default/crashpad_handler ./builddir/pakiki_crashpad_handler
